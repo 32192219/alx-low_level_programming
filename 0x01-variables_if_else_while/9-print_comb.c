8c974d1229446a0da4aa42b2c5f10bdebe91065d
32192219 <benassounada4@gmail.com>
2023-06-17 21:30:08 -0700

    ds

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,28 @@
 /**
  * main - Entry point
  *
+ * Description: print all possible combinations single digit numbers
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-int digit = 0
-while (digit <= 9)
-{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
 	putchar(digit + 48);
 
 	if (digit != 9)
-{
+	{
 	putchar(',');
 	putchar(' ');
-}
-}
+	}
+
+	digit++;
+	}
 	putchar('\n');
+
 	return (0);
 }
