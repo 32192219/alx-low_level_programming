f472fb152b320d6832d8fb77c0c5aa1743e51c78
32192219 <benassounada4@gmail.com>
2023-06-22 12:15:24 -0700

    numbers

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -5,16 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-	return (0);
+int _putchar(char c)
+	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,7 @@
+#ifndef Main_H
+#define Main_H
+#include <stdio.h>
+
+/* Function prototypes for custom functions */
 int _putchar(char c);
+#endif /* MAIN_H */
