3f77f2e99ada186bdd2dfb2816d48e267c9ea6d0
32192219 <benassounada4@gmail.com>
2023-06-24 11:52:10 -0700

    h

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_number(98);
+    print_number(402);
+    print_number(1024);
+    print_number(0);
+    print_number(-98);
+    return (0);
+}
